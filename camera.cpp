0fee8627f61f611143c3a3038851183e6009dd30
jasonochiam <ochiamjason@gmail.com>
2025-09-26 16:02:55 -0500

    fixed glm include and added a little code to main

--- camera.cpp
+++ camera.cpp
@@ -4,18 +4,27 @@
 using camVec3 = glm::vec3;
 using lightVec3 = glm::vec3;
 
+class camAxis{
+    private:
+        camVec3 forward;
+        camVec3 right;
+        camVec3 up;
+    
+    public:
+        camAxis(glm::vec3 x, glm::vec3 y, glm::vec3 z) : forward(z), right(x), up(y){}
+};
+
 class Camera{
     private:
         camVec3 position;
-        camVec3 direction;
-        camVec3 upAxis;
+        camAxis axis;
         int fov;
-        int aspectRatio;
+        float aspectRatio;
 
-    
     public:
-        Camera(glm::vec3 origin, glm::vec3 direction, glm::vec3 upDir, int fov, int aspectRatio) : position(origin), direction(position), upAxis(upDir), fov(fov), aspectRatio(aspectRatio){}
+        Camera(glm::vec3 origin, camAxis axis, int fov, int aspectRatio) : position(origin), axis(axis), fov(fov), aspectRatio(aspectRatio){}
 
         void movePosition(glm::vec3 newPosition){}
         void moveDirection(glm::vec3 newDirection){}
+        void rotateCam(){}
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include "camera.cpp"
 #include "ray.cpp"
+#include <vector>
+
+#define IMAGEX 800
+#define IMAGEY 600
+#define FOV 90
 
 int main(){
     /* 
@@ -22,4 +27,10 @@ int main(){
             - write pixel color to bit buffer (cpu)
             - once all pixels have a computation made, write image
     */
+
+    camAxis mainAxis = camAxis(glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    Camera mainCam = Camera(glm::vec3(0.0f, 0.0f, 0.0f), mainAxis, FOV, IMAGEX/IMAGEY);
+
+    float pixelArray[IMAGEY][IMAGEX] = {0};
+
 }
